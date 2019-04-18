cd44bdb79ce29343a01ad8596e346beaafceee39
Safintim <Sarami96>
2019-04-18 11:47:16 +0300

    fix

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -17,12 +17,10 @@ def get_picture(url, path):
 
 def fetch_hubble_once_image(image_id):
     image_id_url = 'http://hubblesite.org/api/v3/image/{}'
-    filename = 'images/hubble{}.'.format(image_id)
     response = requests.get(image_id_url.format(image_id)).json()
     last_image_url = response['image_files'][-1]['file_url']
-    extension = get_file_extension(last_image_url)
-    new_filename = filename + extension
-    get_picture(last_image_url, new_filename)
+    filename = 'images/hubble{}.'.format(image_id) + get_file_extension(last_image_url)
+    get_picture(last_image_url, filename)
 
 
 def fetch_hubble(collection='wallpaper'):
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -17,11 +17,9 @@ def get_picture(url, path):
 
 def fetch_spacex_last_launch():
     api_url = 'https://api.spacexdata.com/v3/launches/latest'
-    filename = 'images/spacex{}.'
 
     response = requests.get(api_url).json()
     pictures = response['links']['flickr_images']
     for i, image_url in enumerate(pictures):
-        extension = get_file_extension(image_url)
-        new_filename = filename.format(i) + extension
-        get_picture(image_url, new_filename)
+        filename = 'images/spacex{}.'.format(i) + get_file_extension(image_url)
+        get_picture(image_url, filename)
